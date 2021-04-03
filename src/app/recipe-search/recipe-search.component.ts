aefba416caa78455fd5dd76a5a57dc12539add35
phillip.ninan <pninan@lbisoftware.com>
2021-04-03 19:45:17 -0400

    search POC is working

--- src/app/recipe-search/recipe-search.component.ts
+++ src/app/recipe-search/recipe-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-search',
@@ -8,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeSearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  goToRecipe(){
+    this.router.navigate(['/recipe']);
+  }
 }
